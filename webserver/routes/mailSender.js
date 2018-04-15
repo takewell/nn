7b373f56efe0db5373fc16f6fb7a46e66ece8c1c
takewell <5315053@ed.tus.ac.jp>
2018-04-15 22:41:43 +0900

    :heavy_plus_sign: mailSender

---
+++ webserver/routes/mailSender.js
@@ -0,0 +1,30 @@
+const config = require('../config');
+const jwt = require('jsonwebtoken');
+const mailgun = require('mailgun-js')({
+  apiKey: config.MAILGUN_API_KEY,
+  domain: config.MAILGUN_DOMAIN
+});
+
+const send = (email) => {
+  const token = jwt.sign({ email: email }, config.SECRET);
+  const url = config.WEBSERVER_URL_ROOT + 'signup/emailverify?token=' + token;
+
+  const data = {
+    from: 'エヌエヌ動画 <nn@mailgun.org>',
+    to: email, // Need add Recipent fo Mailgun
+    subject: '【エヌエヌ動画】メールアドレスの確認',
+    text: url + '\n以上の URL からアクセスして下さい。'
+  };
+
+  mailgun.messages().send(data, (err, body) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('Mail sended.');
+    console.log(body);
+  });
+
+};
+
+module.exports = send;
