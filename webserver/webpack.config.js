bf775d83aad7e60cba1bc76f85152f6252123259
takewell <5315053@ed.tus.ac.jp>
2018-04-21 22:59:28 +0900

    :heavy_plus_sign:  webpack.config

---
+++ webserver/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './webfront/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './public/javascripts')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['env', { modules: false }], 'react']
+            }
+          }
+        ],
+        // node_modules は除外する
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+    })
+  ],
+  // ソースマップを有効にする
+  devtool: 'source-map'
+};
