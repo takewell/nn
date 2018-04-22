35c41dd7ca260afc37613e3a7cf570d29bcc486b
takewell <5315053@ed.tus.ac.jp>
2018-04-22 15:52:18 +0900

    :heavy_plus_sign: 投稿動画編集ページを実装

--- webserver/routes/my/videos.js
+++ webserver/routes/my/videos.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: true });
 const apiTokenGenerator = require('../../lib/apiTokenGenerator');
 const authenticationEnsurer = require('../authenticationEnsurer');
 const config = require('../../config');
+const Video = require('../../models/video');
+const VideoStatistic = require('../../models/videostatistic');
+const loader = require('../../models/sequelizeLoader');
+const Sequelize = loader.Sequelize;
+const Op = Sequelize.Op;
+
 
 router.get('/', authenticationEnsurer, (req, res, next) => {
   let email = '', apiToken = '';
@@ -13,5 +21,44 @@ router.get('/', authenticationEnsurer, (req, res, next) => {
 
   res.render('my/videos/index', { email: email, apiToken: apiToken, config: config, user: req.user });
 });
+
+router.get('/:videoId', authenticationEnsurer, csrfProtection, async (req, res, next) => {
+  const videoId = req.param.videoId;
+  const video = await Video.findOne({
+    where: {
+      [Op.or]: [
+        { videoId: videoId, videoStatus: 'Published' },
+        { videoId: videoId, videoStatus: 'Encoded' }
+      ]
+    }
+  });
+  if (video && (req.user.isAdmin || video.userId === req.user.userId)) {
+    let email = '';
+    if (req.user) {
+      email = req.user.email;
+    }
+    const formattedCreatedAt = moment(video.createdAt).format(
+      'YYYY/MM/DD HH:mm'
+    );
+    const formattedUpdatedAt = moment(video.updatedAt).format(
+      'YYYY/MM/DD HH:mm'
+    );
+    res.render('my/videos/edit', {
+      email: email,
+      config: config,
+      video: video,
+      formattedCreatedAt: formattedCreatedAt,
+      formattedUpdatedAt: formattedUpdatedAt,
+      user: req.user,
+      csrfToken: req.csrfToken()
+    });
+  } else {
+    res.render('notfoundvideo', {
+      config: config,
+      user: req.user
+    });
+  }
+});
+
 
 module.exports = router;
