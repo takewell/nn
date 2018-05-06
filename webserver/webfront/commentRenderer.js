71afd3e47402015d590950725cbc0c6ae8dff2df
takewell <5315053@ed.tus.ac.jp>
2018-05-06 18:24:48 +0900

    コメントレンダラー詳細を詰める
    衝突防止
    投稿コメントしたコメントは黄色で囲む

--- webserver/webfront/commentRenderer.js
+++ webserver/webfront/commentRenderer.js
@@ -8,7 +8,9 @@ export default class CommentRenderer {
     this.commentListenerContainer = commentListenerContainer;
     this.videoPlayerId = videoPlayerId;
     this.comments = [];
+    this.laneIndexSet = new Set();
     this.objSet = new Set();
+    this.isPlaying = false;
 
     this.fontSize = 24;
     this.controllerHeight = 30;
@@ -57,10 +59,42 @@ export default class CommentRenderer {
     videoContainer.appendChild(this.app.view);
     this.app.view.style = 'position: absolute; top: 0px; left: 0px;';
 
+    this.laneCount = Math.floor(
+      (this.videoPlayer.clientHeight - this.controllerHeight) / this.fontSize
+    );
+
+    this.videoPlayer.addEventListener('play', this.playHandler.bind(this));
+    this.videoPlayer.addEventListener('pause', this.pauseHandler.bind(this));
+    this.videoPlayer.addEventListener('ended', this.pauseHandler.bind(this));
+    this.videoPlayer.addEventListener('seeked', this.seekHandler.bind(this));
+
     this.app.ticker.add(() => {
-      const renderedComments = this.fetchRenderedComment();
-      this.addText(renderedComments);
-      Array.from(this.objSet).forEach(this.updatePosition.bind(this));
+      if (this.isPlaying) {
+        const renderedComments = this.fetchRenderedComment();
+        this.addText(renderedComments);
+        Array.from(this.objSet).forEach(this.updatePosition.bind(this));
+      }
+    });
+  }
+
+  playHandler(event) {
+    const renderedComments = this.fetchRenderedComment();
+    this.addText(renderedComments);
+    this.isPlaying = true;
+  }
+
+  pauseHandler(event) {
+    this.isPlaying = false;
+  }
+
+  seekHandler(event) {
+    this.laneIndexSet = new Set();
+    Array.from(this.objSet).forEach(obj => {
+      obj.destroy();
+      this.objSet.delete(obj);
+    });
+    this.comments.forEach(comment => {
+      comment.isFetched = false; // シークしたら描画用フェッチを一旦リセット
     });
   }
 
@@ -87,27 +121,56 @@ export default class CommentRenderer {
     return renderedComments;
   }
 
+  popRandomFromSet() {
+    if (this.laneIndexSet.size === 0) {
+      // 中身がない時は初期化
+      for (let i = 0; i < this.laneCount; i++) {
+        this.laneIndexSet.add(i);
+      }
+    }
+    const randomIndex = Math.floor(Math.random() * this.laneIndexSet.size);
+    const index = Array.from(this.laneIndexSet)[randomIndex];
+    this.laneIndexSet.delete(index);
+    return index;
+  }
+
   addText(renderedComments) {
     renderedComments.forEach(comment => {
       const text = new PIXI.Text(comment.content, this.textStyle);
       this.objSet.add(text);
+      const index = this.popRandomFromSet();
+      const y = index * this.fontSize;
       const cWidth = this.videoPlayer.clientWidth;
 
-      text.y = 0;
+      text.y = y;
       text.x = cWidth;
       this.app.stage.addChild(text);
+
+      if (comment.isSelfPosted) {
+        // 自身の投稿に黄色い枠線を付ける
+        const graphics = new PIXI.Graphics();
+        graphics.lineStyle(1, 0xffff00, 1);
+        graphics.drawRect(0, 0, text.width, text.height);
+        const sprite = new PIXI.Sprite(graphics.generateCanvasTexture());
+        sprite.x = text.x;
+        sprite.y = text.y;
+        this.objSet.add(sprite);
+        this.app.stage.addChild(sprite);
+      }
+
     });
   }
 
   updatePosition(obj) {
-    const moveDiff = -3;
-    obj.x += moveDiff; // 左少し移動
+    if (this.isPlaying) {
+      const moveDiff = -3;
+      obj.x += moveDiff; // 左少し移動
 
-    if (obj.x < -obj.width) {
-      // テキストが左側で消えたらTextを消滅させる
-      obj.destroy();
-      this.objSet.delete(obj);
+      if (obj.x < -obj.width) {
+        // テキストが左側で消えたらTextを消滅させる
+        obj.destroy();
+        this.objSet.delete(obj);
+      }
     }
   }
-
 }
